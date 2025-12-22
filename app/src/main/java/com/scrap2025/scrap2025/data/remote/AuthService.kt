99119e4dc212a144e5804ef89a91d4df682fe581
HyunjeLee <cyeclamen07@gmail.com>
2025-12-22 21:54:18 +0900

    feat: mypage, logout, signout(withdraw) api 구현

--- app/src/main/java/com/scrap2025/scrap2025/data/remote/AuthService.kt
+++ app/src/main/java/com/scrap2025/scrap2025/data/remote/AuthService.kt
@@ -2,7 +2,9 @@ package com.scrap2025.scrap2025.data.remote
 
 import com.scrap2025.scrap2025.data.model.BaseResponse
 import com.scrap2025.scrap2025.data.model.LoginResult
+import com.scrap2025.scrap2025.data.model.MyPageResult
 import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
@@ -11,13 +13,27 @@ interface AuthService {
     @retrofit2.http.Headers("accept: application/json")
     @POST("/oauth/login")
     suspend fun login(
-            @Query("sns") sns: String,
-            @Header("Authorization") token: String
+        @Query("sns") sns: String,
+        @Header("Authorization") token: String
     ): Response<BaseResponse<LoginResult>>
 
     @retrofit2.http.Headers("accept: application/json")
     @POST("/token")
     suspend fun refreshToken(
-            @Query("refresh_token") refreshToken: String
+        @Query("refresh_token") refreshToken: String
     ): Response<BaseResponse<LoginResult>>
+
+    @retrofit2.http.Headers("accept: application/json")
+    @GET("/auth/mypage")
+    suspend fun getMyPage(
+        @Header("Authorization") token: String
+    ): Response<BaseResponse<MyPageResult>>
+
+    @retrofit2.http.Headers("accept: application/json")
+    @POST("/auth/logout")
+    suspend fun logout(@Header("Authorization") token: String): Response<BaseResponse<Unit?>>
+
+    @retrofit2.http.Headers("accept: application/json")
+    @POST("/auth/signout")
+    suspend fun withdraw(@Header("Authorization") token: String): Response<BaseResponse<Unit?>>
 }
