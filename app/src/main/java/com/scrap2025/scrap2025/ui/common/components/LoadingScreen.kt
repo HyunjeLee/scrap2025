319ca818b8049523719a9f04222de840ae79d01e
HyunjeLee <cyeclamen07@gmail.com>
2025-12-30 00:48:07 +0900

    fix: 누락된 LoadingScreen 구현

--- app/src/main/java/com/scrap2025/scrap2025/ui/common/components/LoadingScreen.kt
+++ app/src/main/java/com/scrap2025/scrap2025/ui/common/components/LoadingScreen.kt
@@ -1,8 +1,18 @@
 package com.scrap2025.scrap2025.ui.common.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.scrap2025.scrap2025.ui.theme.MainColor
+import com.scrap2025.scrap2025.ui.theme.MainColorDeep
 
 @Composable
 fun LoadingScreen() {
-
+    Box(modifier = Modifier.fillMaxSize().background(MainColor), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator(color = MainColorDeep)
+    }
 }
