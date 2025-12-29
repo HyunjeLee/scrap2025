0ef9e03dd12307e41e5267ce440f755d6fb3b12c
HyunjeLee <cyeclamen07@gmail.com>
2025-12-30 00:53:08 +0900

    feat: 자동 로그인 구현 #49
    close #49

--- app/src/main/java/com/scrap2025/scrap2025/navigation/AppNavHost.kt
+++ app/src/main/java/com/scrap2025/scrap2025/navigation/AppNavHost.kt
@@ -1,18 +1,32 @@
 package com.scrap2025.scrap2025.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.scrap2025.scrap2025.ui.common.components.LoadingScreen
 import com.scrap2025.scrap2025.ui.theme.Scrap2025Theme
+import com.scrap2025.scrap2025.viewmodel.MainViewModel
 
 @Composable
-fun AppNavHost() {
+fun AppNavHost(
+    mainViewModel: MainViewModel = hiltViewModel()
+) {
     val navController = rememberNavController()
+    val accessToken by mainViewModel.accessToken.collectAsState()
+
+    if (accessToken == "") { // 토큰을 읽어오는 중
+        return LoadingScreen()
+    }
+
+    val startRoute = if (accessToken != null) Main else Login
 
     NavHost(
         navController = navController,
-        startDestination = Login
+        startDestination = startRoute
     ) {
         mainNavGraph(navController)
     }
